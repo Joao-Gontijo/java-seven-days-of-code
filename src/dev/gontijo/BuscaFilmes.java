d15a2e3302021354c90349e1a538cbbdddd6e7c7
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-03 15:40:43 -0300

    refatoração para classe HTMLGenerator

--- src/dev/gontijo/BuscaFilmes.java
+++ src/dev/gontijo/BuscaFilmes.java
@@ -1,8 +1,6 @@
 package dev.gontijo;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URI;
@@ -11,7 +9,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.json.JSONArray;
@@ -50,35 +47,9 @@ public class BuscaFilmes {
 		File f = new File("html//source.html");
 		PrintWriter writer = new PrintWriter(f);
 		
-		
-		String head =
-				"""
-				<head>
-					<meta charset=\"utf-8\">
-					<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
-					<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css\" 
-						+ "integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">					
-				</head>
-				""";
-		String titulo = "<div><h1>Top 250 Filmes</h1></div>";
-		
-		writer.write(head);
-		writer.write(titulo);
-		
-		
-		for (Iterator<Filme> iterator = listaFilmes.iterator(); iterator.hasNext();) {
-			Filme filme = (Filme) iterator.next();
-			String card = "<div class=\"card\" style=\"width: 14rem;\">\r\n"
-					+ "  <img class=\"card-img-top\" src=\""+ filme.getUrlImagem() +"\"\" alt=\"Card image cap\">\r\n"
-					+ "  <div class=\"card-body\">\r\n"
-					+ "    <h5 class=\"card-title\">" + filme.getTitulo() + "</h5>\r\n"
-					+ "    <p class=\"card-text\"> Nota: " + filme.getNota() + "- Ano: " + filme.getAno() + "</p>\r\n"
-					+ "  </div>\r\n"
-					+ "</div>";
-			
-			writer.write(card);
-		}
-		
+		HTMLGenerator geradorHtml = new HTMLGenerator();
+		geradorHtml.generate(listaFilmes, writer);
 		writer.close();
+		
 	}
 }
--- src/dev/gontijo/HTMLGenerator.java
+++ src/dev/gontijo/HTMLGenerator.java
@@ -1,17 +1,14 @@
 package dev.gontijo;
 
 import java.io.PrintWriter;
+import java.util.Iterator;
 import java.util.List;
 
 import dev.gontijo.model.Filme;
 
 public class HTMLGenerator {
 	
-	public HTMLGenerator(PrintWriter writer) {
-		
-	}
-	
-	public void generate(List<Filme> lista) {
+	public void generate(List<Filme> lista, PrintWriter writer) {
 		String head =
 				"""
 				<head>
@@ -21,6 +18,21 @@ public class HTMLGenerator {
 						+ "integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">					
 				</head>
 				""";
+		String titulo = "<div><h1>Top 250 Filmes</h1></div>";
 		
+		writer.write(head);
+		writer.write(titulo);
+		
+		for (Iterator<Filme> iterator = lista.iterator(); iterator.hasNext();) {
+			Filme filme = (Filme) iterator.next();
+			String card = "<div class=\"card\" style=\"width: 14rem;\">\r\n"
+					+ "  <img class=\"card-img-top\" src=\""+ filme.getUrlImagem() +"\"\" alt=\"Card image cap\">\r\n"
+					+ "  <div class=\"card-body\">\r\n"
+					+ "    <h5 class=\"card-title\">" + filme.getTitulo() + "</h5>\r\n"
+					+ "    <p class=\"card-text\"> Nota: " + filme.getNota() + "- Ano: " + filme.getAno() + "</p>\r\n"
+					+ "  </div>\r\n"
+					+ "</div>";
+			writer.write(card);
+		}	
 	}
 }
